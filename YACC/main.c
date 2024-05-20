a8dd9b795dde59ce5f16da4f1ecc28b386a7afb6
JoongHyun Kim <joongh@ewhain.net>
2024-05-20 14:24:36 +0900

    Add files via upload

---
+++ YACC/main.c
@@ -0,0 +1,7 @@
+void main()
+{
+	printf("start of parser\n");
+	yyparse();
+	printf("end of parser\n");
+	printf("JoongHyun Kim(2076088) Seojin Kwak(2076016) Seonyeong Kim(2071010) NaHyun Lee(2076292) \n");
+}
