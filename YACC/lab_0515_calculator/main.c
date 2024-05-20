df3a54a36f7b092eeeb02ea90f89453d3d5b9f56
JoongHyun Kim <joongh@ewhain.net>
2024-05-20 11:49:07 +0900

    Update main.c

--- YACC/lab_0515_calculator/main.c
+++ YACC/lab_0515_calculator/main.c
@@ -2,5 +2,7 @@
 extern int yyparse();
 int main()
 {
-	return yyparse();
+	yyparse();
+	printf("JoongHyun Kim(2076088) Seojin Kwak(2076016) Seonyeong Kim(2071010) NaHyun Lee(2076292) \n");
+	return 0;
 }
