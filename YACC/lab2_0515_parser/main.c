1564a386a29a671d396b6b2e8638b439807d547f
Seojin Kwak <81500249+SJ-Kwak@users.noreply.github.com>
2024-05-20 22:52:31 +0900

    lab02

--- YACC/lab2_0515_parser/main.c
+++ YACC/lab2_0515_parser/main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+extern int yyparse();
 void main()
 {
 	printf("start of parser\n");
---
+++ YACC/lab2_0515_parser/tn.h
@@ -0,0 +1,29 @@
+#ifndef YYSTYPE
+#define YYSTYPE int
+#endif
+#define	tident	258
+#define	tnumber	259
+#define	tconst	260
+#define	telse	261
+#define	tif	262
+#define	tint	263
+#define	treturn	264
+#define	tvoid	265
+#define	twhile	266
+#define	taddAssign	267
+#define	tsubAssign	268
+#define	tmulAssign	269
+#define	tdivAssign	270
+#define	tmodAssign	271
+#define	tor	272
+#define	tand	273
+#define	tequal	274
+#define	tnotequ	275
+#define	tgreate	276
+#define	tlesse	277
+#define	tinc	278
+#define	tdec	279
+#define	LOWER_THAN_ELSE	280
+
+
+extern YYSTYPE yylval;
