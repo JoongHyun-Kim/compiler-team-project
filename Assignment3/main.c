bc4d31ea856e3bfaefebe8b1a04f62f540621560
JoongHyun Kim <joongh@ewhain.net>
2024-05-30 23:07:54 +0900

    Update main.c

--- Assignment3/main.c
+++ Assignment3/main.c
@@ -14,15 +14,20 @@ extern void PrintHStable();
 
 void main()
 {
+    cLine = 1;
+
     printf("\n\t[Error Report : Error Information]");
     printf("\n=================================================\n");
     printf("*** MiniC parsing begins\n\n");
-    cLine = 1;
+    
     yyparse();
+    
     printf("\nParsing ends. ***\n");
+    
     if (cErrors == 0) printf("\nno error detected!\n");
     else printf("\n%d errors detected!\n", cErrors);
     printf("\n=================================================\n");
+    
     PrintHStable(); // identifier와 그 type을 print하는 함수
     printf("JoongHyun Kim(2076088) Seojin Kwak(2076016) Seonyeong Kim(2071010) NaHyun Lee(2076292) \n");
 }
